92810a61bf4af1630da5ff9c7b73d2d07c0081e6
Yuji Sugiura <y.sugiura.0316@gmail.com>
2022-01-31 21:00:13 +0900

    Init nuxt3

---
+++ pages/foo.vue
@@ -0,0 +1,7 @@
+<script setup>
+  const { data } = await useAsyncData('count', () => Promise.resolve(Math.random()))
+</script>
+
+<template>
+  <p>{{data || "N/A"}}</p>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,6 @@
+<template>
+  <h2>Hello Nuxt3</h2>
+  <hr />
+  <NuxtLink to="/foo">foo</NuxtLink>
+</template>
+
